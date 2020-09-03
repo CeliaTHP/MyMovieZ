da23cf6de6d0949cb353829755ae52cb1b11e6d5
CeliaTHP <celia.95190@gmail.com>
2020-09-03 21:53:34 +0200

    Splash + listing inc

---
+++ app/src/main/java/fr/ay/mymoviez/MainActivity.java
@@ -0,0 +1,32 @@
+package fr.ay.mymoviez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView go;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.go = findViewById(R.id.go);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent otherActivity = new Intent(getApplicationContext(), Listing.class);
+                startActivity(otherActivity);
+                finish();
+
+            }
+        });
+    }
+}
